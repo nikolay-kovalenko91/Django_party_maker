a384709328b5db45491ea35ccff99d9ce624d21a
nikolay-kovalenko91 <nikolay.kovalenko91@gmail.com>
2016-08-09 10:27:30 +0700

    test

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import SocialTypes, UserSocialProfile
+from .models import SocialTypes, UserSocialProfile, Poll
 
 admin.site.register(SocialTypes)
 admin.site.register(UserSocialProfile)
+admin.site.register(Poll)
