7248b86232515d6e3cacd7ca7ecf217ade874b2a
nikolay-kovalenko91 <nikolay.kovalenko91@gmail.com>
2016-08-09 15:24:17 +0700

    test

--- party_maker/urls.py
+++ party_maker/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('polls.urls'))
 ]
+urlpatterns += staticfiles_urlpatterns()
