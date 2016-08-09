1ef79de4e285fe4836e7b77a7c3fc78e13d24dd1
nikolay-kovalenko91 <nikolay.kovalenko91@gmail.com>
2016-08-09 19:47:56 +0700

    test

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^login', views.login_page, name='login'),
     url(r'^vk', views.vk_handler, name='vk_handler'),
     url(r'^new_poll', views.new_poll, name='new_poll'),
