85c32cfb6d3421ddf1df4525b7aae40325e49c66
nikolay-kovalenko91 <nikolay.kovalenko91@gmail.com>
2016-08-09 19:06:53 +0700

    test

--- polls/urls.py
+++ polls/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^new_poll', views.new_poll, name='new_poll'),
     url(r'^polls_list', views.polls_list, name='polls_list'),
     url(r'^vote_end', views.vote_end, name='vote_end'),
-    url(r'^confirm_change_poll', views.confirm_change_poll, name='confirm_change_poll'),
+    url(r'^confirm_change_poll$', views.confirm_change_poll, name='confirm_change_poll'),
 ]
